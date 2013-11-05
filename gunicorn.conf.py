a2076f9774b3908bd1a4764aa246a0a9fea5f804
Peter Bouda <pbouda@cidles.eu>
2013-11-05 12:31:23 +0000

    Updated number of workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import os
 
 bind = "127.0.0.1:8200"
-workers = (os.sysconf("SC_NPROCESSORS_ONLN") * 2) + 1
+workers = 2
 #loglevel = "error"
 proc_name = "poio"
 logfile = '/home/sites/poio/debug.log'
